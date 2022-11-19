b526c9aba4abea6dbb582a83fffd02133e1d926d
MakarovOleg1988 <makarov.oleg1988@gmail.com>
2022-11-19 15:43:34 +0530

    Добавьте файлы проекта.

---
+++ CatchExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CatchExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tours = { "England", "Spain", "Italy", "Portugal", "France" };
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(tours[choice]);
+            }
+            catch
+            {
+                Console.WriteLine("Wrong number");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye");
+            }
+        }
+    }
+}
